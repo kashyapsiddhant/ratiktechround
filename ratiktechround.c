6aeeabb1d0fd6fad8ccc4f2f1fcb6bcb416a4873
Siddhant Kashyap <103328725+SiddhantGitBoy@users.noreply.github.com>
2022-04-27 01:15:11 -0700

    Create ratiktechround.c

---
+++ ratiktechround.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int testnum1,testnum2;
+    int sum,sub,mult,mod;
+    float div;
+    scanf("%d %d",&testnum1, &testnum2);
+    sum=testnum1+testnum2;
+    sub=testnum1-testnum2;
+    mult=testnum1*testnum2;
+    div=(float)testnum1/testnum2;
+    mod=testnum1%testnum2;
+    printf("Addition : %d\nSubtraction : %d\nMultiplication : %d\nDivision : %0.3f\nModulus : %d",sum, sub, mult, div, mod);
+   
+	return 0;
+}
